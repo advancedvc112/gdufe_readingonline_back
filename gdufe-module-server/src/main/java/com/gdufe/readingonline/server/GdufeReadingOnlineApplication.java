0825b8a17472af11768e11b0b283e6bbc5836c3b
advancedvc112 <advancedvc_112@qq.com>
2025-10-18 12:19:19 +0800

    (feat):excel文件解析和导入数据库

--- gdufe-module-server/src/main/java/com/gdufe/readingonline/server/GdufeReadingOnlineApplication.java
+++ gdufe-module-server/src/main/java/com/gdufe/readingonline/server/GdufeReadingOnlineApplication.java
@@ -3,9 +3,11 @@ package com.gdufe.readingonline.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.gdufe.readingonline.dal.mysqlmapper")
 @SpringBootApplication
+@ComponentScan(basePackages = "com.gdufe.readingonline")
 public class GdufeReadingOnlineApplication {
     public static void main(String[] args) {
         SpringApplication.run(GdufeReadingOnlineApplication.class, args);
